41ed4d60d5cc2fd9bd35b3f808c7be71abe49468
Joaquin Ferroni <joaco.790@gmail.com>
2023-02-21 20:50:52 +0100

    allow user to remove dates after 24 hours and fix styling

--- src/app/_pipes/prototypes.ts
+++ src/app/_pipes/prototypes.ts
@@ -3,6 +3,7 @@ declare global{
 
   interface Date {
     equalTo(compareTo:Date): boolean;
+    diffInHours(compareTo:Date):number;
   }
 }
 
@@ -12,3 +13,9 @@ Date.prototype.equalTo = function (compareTo:Date) {
   && this.getMonth() === compareTo.getMonth()
   && this.getDate() === compareTo.getDate()
 };
+
+Date.prototype.diffInHours = function( compareTo: Date){
+  var diff =(this.getTime() - compareTo.getTime()) / 1000;
+  diff /= (60 * 60);
+  return Math.abs(Math.round(diff));
+}
