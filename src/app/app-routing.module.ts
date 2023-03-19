1a350d7e6bceac4be9d12473c6afc1b108f20b48
Joaquin Ferroni <joaco.790@gmail.com>
2023-03-19 21:00:23 +0100

    add privacy politics to site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,10 @@ const routes: Routes = [
     path:'', loadChildren: ()=> import('./home/home.module')
     .then(m=>m.HomeModule)
   },
-  
+  {
+    path:'privacy-policies', loadChildren: ()=> import('./privacy-policies/privacy-policies.module')
+    .then(m=>m.PrivacyPoliciesModule)
+  },
   { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }
   ,{
     path:'**', loadChildren: ()=> import('./home/home.module')
