f9ac4fcfcb77220570346c970b4468c761447aac
Joaquin Ferroni <joaco.790@gmail.com>
2023-03-19 22:35:51 +0100

    fix ui issues

--- src/app/payments/payment-debt-dialog.component.ts
+++ src/app/payments/payment-debt-dialog.component.ts
@@ -18,6 +18,7 @@ export class PaymentDebtInput {
 })
 export class PaymentDebtDialog {
   appointments: Appointment[] = [];
+  today = new Date();
   
   constructor(
     private appointmentService: AppointmentService,
