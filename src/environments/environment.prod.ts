cf09a6472483ef982f3de174cce82f92aeed0e91
Joaquin Ferroni <joaco.790@gmail.com>
2024-02-05 09:38:55 +0100

    change videocall token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   secretKey: "psicoe84ad660c4721ae0e84ad660c4721ae0fer",
   apiUrl:"https://psicofernandagarcia.com:8443/api",
-  video_appid: "f93068a15aca4d4abb1247a410bb4b7d"
+  video_appid: "fe9914cd6e904bf892e49119a3bc16d4"
 };
